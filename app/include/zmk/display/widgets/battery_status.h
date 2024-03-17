27f1c111ed61c6015d40f81541f33a8378c4a835
Mr.Catfood <sipos22@msn.com>
2024-03-17 20:29:41 +0100

    Update battery_status.h

--- app/include/zmk/display/widgets/battery_status.h
+++ app/include/zmk/display/widgets/battery_status.h
@@ -16,3 +16,6 @@ struct zmk_widget_battery_status {
 
 int zmk_widget_battery_status_init(struct zmk_widget_battery_status *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_battery_status_obj(struct zmk_widget_battery_status *widget);
+
+int zmk_widget_peripheral_battery_status_init(struct zmk_widget_battery_status *peripheral_widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_peripheral_battery_status_obj(struct zmk_widget_battery_status *peripheral_widget);
